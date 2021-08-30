282f2ac4dc544cbaea04c86690de1106caa486d8
JeanAQ <jean.ascona@autonomadeica.edu.pe>
2021-08-29 21:35:01 -0500

    mi primera vez

---
+++ js/persona.js
@@ -0,0 +1,19 @@
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        return this._apellido = apellido;
+    }
+    
+}
